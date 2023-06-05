3d869af83955f4315e05de16ee8460171cfdcc33
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-06-05 19:30:45 +0300

    very tiny tune 05/06

--- MyApp/App.js
+++ MyApp/App.js
@@ -3,12 +3,15 @@ import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './src/Screens/Home'
 import {MaterialCommunityIcons} from '@expo/vector-icons'
+import Login from './src/Screens/Login';
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name = "Home" component={Home} options={{tabBarLabel:'Home',tabBarIcon:()=>(<MaterialCommunityIcons name="home" size={35} color="black"/>)}}/>
+        <Tab.Screen name = "Login" component={Login} options={{tabBarLabel:'Login',tabBarIcon:()=>(<MaterialCommunityIcons name="Login" size={35} color="black"/>)}}/>
+        <Tab.Screen name = "SignUp" component={SignUp} options={{tabBarLabel:'SignUp',tabBarIcon:()=>(<MaterialCommunityIcons name="SignUp" size={35} color="black"/>)}}/>
       </Tab.Navigator>
 
     </NavigationContainer>
