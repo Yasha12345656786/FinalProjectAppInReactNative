3cf92babf1afaf6d534810fb72a04ec7ff419246
Yasha12345656786 <117259949+Yasha12345656786@users.noreply.github.com>
2023-09-14 18:33:32 +0300

    Update App.js

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,23 +1,20 @@
-
-import { StyleSheet } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/Screens/Home'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import Login from './src/Screens/Login';
-import SignUp from './src/Screens/SignUp';
-import Menu from './src/Screens/Menu';
-import TriviaGame from './src/Screens/TriviaGame';
-import PlayerContextProvider from './src/Context/PlayerContext';
-import TriviaContextProvider from './src/Context/TriviaContext';
-import ForgotPassword from './src/Screens/ForgotPassword';
-import ForgotUsername from './src/Screens/ForgotUsername';
+import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/Screens/Home";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Login from "./src/Screens/Login";
+import SignUp from "./src/Screens/SignUp";
+import Menu from "./src/Screens/Menu";
+import TriviaGame from "./src/Screens/TriviaGame";
+import PlayerContextProvider from "./src/Context/PlayerContext";
+import TriviaContextProvider from "./src/Context/TriviaContext";
+import ForgotPassword from "./src/Screens/ForgotPassword";
+import ForgotUsername from "./src/Screens/ForgotUsername";
 
 const Stack = createStackNavigator();
-
-
 
 const MainMenu = () => {
   return (
@@ -30,30 +27,26 @@ const MainMenu = () => {
         <Tab.Screen name="Trivia" component={TriviaGame} options={{ tabBarLabel: 'Question', tabBarIcon: () => (<MaterialCommunityIcons name="Question" size={35} color="black" />) }} />
       </Tab.Navigator> */}
       <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home}/>
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword}/>
-      <Stack.Screen name="ForgotUsername" component={ForgotUsername}/>
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="SignUp" component={SignUp}/>
-      <Stack.Screen name="Trivia" component={TriviaGame}/>
-      <Stack.Screen name="Menu" component={Menu}/>
-    </Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="ForgotUsername" component={ForgotUsername} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Trivia" component={TriviaGame} />
+        <Stack.Screen name="Menu" component={Menu} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
-
+  );
+};
 
 export default function App() {
   return (
-
     <>
       <PlayerContextProvider>
         <TriviaContextProvider>
           <MainMenu />
         </TriviaContextProvider>
       </PlayerContextProvider>
-
-
     </>
   );
 }
@@ -61,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
